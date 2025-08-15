f7af0481aadcea0ff5f6bf28f925a4c411f1bdeb
Phạm Tấn Nghĩa <139458255+JakePham23@users.noreply.github.com>
2025-08-15 19:08:01 +0700

    fix error result page

---
+++ src/app/exam/results/page.tsx
@@ -0,0 +1,17 @@
+// src/app/exam/results/[resultId]/page.tsx
+
+// Import necessary modules
+import React from 'react';
+
+// Define the component
+const page = () => {
+  return (
+    <div>
+      <h1>Kết quả bài thi</h1>
+      <p>Nội dung trang kết quả...</p>
+    </div>
+  );
+};
+
+// Export the component as the default export
+export default page;
